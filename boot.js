e0df09b9f604c678fd67acee16fe77a3ee13898d
Tomasz Libich <tomasz.libich@libtom.com>
2017-06-16 16:53:25 +0100

    Adding SSL

--- boot.js
+++ boot.js
@@ -1,12 +1,33 @@
-const http = require('http'),
+const fs = require('fs-extra'),
+    http = require('http'),
+    https = require('https'),
     express = require('express'),
     bodyParser = require('body-parser'),
     app = express(),
     api = require('./api'),
     cors = require('./cors'),
     responder = require('./responder'),
-    port = process.env.SERVER_PORT || 5000,
-    fileId = '[boot]';
+    httpPort = process.env.SERVER_PORT || 5000,
+    httpsPort = process.env.SECURE_SERVER_PORT || 5001,
+    fileId = '[boot]',
+    serverKeyPath = './ssl/server.key',
+    serverCertPath = './ssl/server.crt',
+    serverKeyExists = require('fs-extra').existsSync(serverKeyPath) ? true : false, //eslint-disable-line no-sync
+    serverCertExists = require('fs-extra').existsSync(serverCertPath) ? true : false; //eslint-disable-line no-sync
+
+let privateKey, certificate, sslOptions;
+
+if (serverKeyExists && serverCertExists) {
+    console.log(fileId, 'server.key and server.crt files detected, loading...');
+    privateKey = fs.readFileSync(serverKeyPath, 'utf8'), //eslint-disable-line
+    certificate = fs.readFileSync(serverCertPath, 'utf8'), //eslint-disable-line
+    sslOptions = {
+        key: privateKey,
+        cert: certificate,
+        requestCert: false,
+        rejectUnauthorized: false
+    };
+}
 
 class Boot {
     init() {
@@ -16,14 +37,18 @@ class Boot {
             extended: false
         }));
 
+        http.createServer(app).listen(httpPort, () => {
+            console.log(`${fileId} Running http server on port ${httpPort}`);
+        });
+
+        https.createServer(sslOptions, app).listen(httpsPort, () => {
+            console.log(`${fileId} Running https server on port ${httpsPort}`);
+        });
+
         app.post('/api/mail/', api.handle);
         app.use((request, response, next) => {
             responder.reject(response);
             next();
-        });
-
-        http.createServer(app).listen(port, () => {
-            console.log(`${fileId} Running http server on port ${port}`);
         });
     }
 }
--- cors.js
+++ cors.js
@@ -1,5 +1,5 @@
 module.exports = function (request, response, next) {
-    response.header('Access-Control-Allow-Origin', 'http://tomaszlibich.net');
+    response.header('Access-Control-Allow-Origin', 'http://tomaszlibich.net https://tomaszlibich.net https://data-segment-148123.firebaseapp.com');
     response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     response.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,x-system-access-key,withCredentials');
 
