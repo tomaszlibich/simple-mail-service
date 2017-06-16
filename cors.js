10dccb6a42d92fe4e3e3304bb0f939235412a0dc
Tomasz Libich <tomasz.libich@libtom.com>
2017-06-16 17:13:42 +0100

    Modifying CORS

--- cors.js
+++ cors.js
@@ -1,6 +1,5 @@
 module.exports = function (request, response, next) {
     response.header('Access-Control-Allow-Origin', 'https://tomaszlibich.net');
-    response.header('Access-Control-Allow-Origin', 'https://data-segment-148123.firebaseapp.com');
     response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     response.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,x-system-access-key,withCredentials');
 
